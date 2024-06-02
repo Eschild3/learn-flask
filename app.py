bb9c736be92857eda85c4f85d2cd13b020a8e94c
Eschild3 <38532574+Eschild3@users.noreply.github.com>
2024-06-02 15:59:10 -0400

    Added logging on request and response

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from http import HTTPStatus
+import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 app = Flask(__name__)
+
+
+@app.before_request
+def before():
+    logger.info("Request: {\n\tMETHOD: %s, \n\tURL: %s, \n\tARGS: %s, \n\tDATA: %s \n}",
+                request.method, request.url, request.args, request.data)
+
+
+@app.after_request
+def after(response):
+    logger.info("Response: {\n\tSTATUS: %s\n\tDATA: %s\n}",
+                response.status_code, response.get_data(as_text=True).rstrip())
+    return response
 
 
 @app.route("/hello/", methods=["GET", "POST"])
