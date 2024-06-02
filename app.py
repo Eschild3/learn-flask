47b793c93048bc0554ad745fd89865fee244e01f
Eschild3 <38532574+Eschild3@users.noreply.github.com>
2024-06-02 10:46:12 -0400

    Added endpoints for testing variable sections in URLs

--- app.py
+++ app.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 
 @app.route("/hello/", methods=["GET", "POST"])
 def welcome():
-    return "Hello World"
+    return "Hello World!"
+
+
+@app.route("/increment/<int:number>/", methods=["GET"])
+def increment(number):
+    return f"Incremented Number: {str(number + 1)}"
+
+
+@app.route("/greeting/<string:name>/", methods=["GET"])
+def greet(name):
+    return f"Hello {name}!"
 
 
 if __name__ == "__main__":
