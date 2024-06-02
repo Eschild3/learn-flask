f0f32c3bb07055aa188e72ff8aeaa0826009449b
Eschild3 <38532574+Eschild3@users.noreply.github.com>
2024-06-02 10:59:21 -0400

    Added HTTP status codes to responses

--- app.py
+++ app.py
@@ -1,31 +1,32 @@
 from flask import Flask, jsonify
+from http import HTTPStatus
 
 app = Flask(__name__)
 
 
 @app.route("/hello/", methods=["GET", "POST"])
 def welcome():
-    return "Hello World!"
+    return "Hello World!", HTTPStatus.OK
 
 
 @app.route("/increment/<int:number>/", methods=["GET"])
 def increment(number):
-    return f"Incremented Number: {str(number + 1)}"
+    return f"Incremented Number: {str(number + 1)}", HTTPStatus.OK
 
 
 @app.route("/greeting/<string:name>/", methods=["GET"])
 def greet(name):
-    return f"Hello {name}!"
+    return f"Hello {name}!", HTTPStatus.OK
 
 
 @app.route("/person/", methods=["GET"])
 def person():
-    return jsonify({"firstName": "EJ", "lastName": "Schildnecht"})
+    return jsonify({"firstName": "EJ", "lastName": "Schildnecht"}), HTTPStatus.OK
 
 
 @app.route("/numbers/<int:length>/", methods=["GET"])
 def numbers(length):
-    return jsonify(list(range(length)))
+    return jsonify(list(range(length))), HTTPStatus.OK
 
 
 if __name__ == "__main__":
