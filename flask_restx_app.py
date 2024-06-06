c15481dd3f0b2571cc872b954a50bffc32169c33
Eschild3 <38532574+Eschild3@users.noreply.github.com>
2024-06-05 22:13:59 -0400

    * Added the file namespace and endpoint for testing file upload

--- flask_restx_app.py
+++ flask_restx_app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restx import Api, Resource, Namespace, reqparse
+from werkzeug.datastructures import FileStorage
 from http import HTTPStatus
 
 app = Flask(__name__)
@@ -21,10 +22,10 @@ class Test(Resource):
 
 api.add_namespace(test_ns, path="/test")
 
+hello_ns = Namespace(name="hello", description="This is the hello namespace")
+
 hello_parser = reqparse.RequestParser()
 hello_parser.add_argument("name", help="Please enter a name")
-
-hello_ns = Namespace(name="hello", description="This is the hello namespace")
 
 
 @hello_ns.route("/")
@@ -38,6 +39,20 @@ class Hello(Resource):
 
 
 api.add_namespace(hello_ns, path="/hello")
+
+file_ns = Namespace(name="file", description="This is an endpoint for working with files")
+
+file_parser = reqparse.RequestParser()
+file_parser.add_argument("file", location="files", type=FileStorage, help="For use with files")
+
+
+@file_ns.route("/")
+class File(Resource):
+    def post(self):
+        args = file_parser.parse_args()
+        file = args.get("file")
+        return f"Uploaded {file.filename}!"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
